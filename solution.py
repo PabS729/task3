bf9abd4165850f7b435e8868bf9ee37d83c49707
Minjing <mishi@ucsd.edu>
2023-11-10 12:25:20 +0100

    s

--- solution.py
+++ solution.py
@@ -1,8 +1,9 @@
 import numpy as np
-from scipy.optimize import fmin_l_bfgs_b, NonlinearConstraint, minimize
+from scipy.optimize import fmin_l_bfgs_b, NonlinearConstraint, minimize, Bounds, fmin_cobyla
 from sklearn import gaussian_process as gp
 import warnings
 from scipy.stats import norm
+from matplotlib import pyplot as plt
 
 DOMAIN = np.array([[0, 10]])  # restrict \theta in [0, 10]
 SAFETY_THRESHOLD = 4  # threshold, upper bound of SA
@@ -16,18 +17,20 @@ class BO_algo():
     def __init__(self):
         """Initializes the algorithm with a parameter configuration. """
 
-        self.beta = 1
+        self.beta = 4
         self.beta_v = 4
+        self.v_mean = 4
         # self.mean = 4
         # self.var = 0.0001
-        self.gpf = gp.GaussianProcessRegressor(kernel = 0.5 * gp.kernels.Matern(length_scale=1, nu=2.5, length_scale_bounds="fixed"))
-        self.gpv = gp.GaussianProcessRegressor(kernel = np.sqrt(2) * gp.kernels.Matern(length_scale=1,nu=2.5, length_scale_bounds="fixed"))
+        self.gpf = gp.GaussianProcessRegressor(kernel = 0.5 * gp.kernels.Matern(length_scale=1, nu=2.5, length_scale_bounds="fixed"), alpha=0.15)
+        self.gpv = gp.GaussianProcessRegressor(kernel = 4 + np.sqrt(2) * gp.kernels.Matern(length_scale=1,nu=2.5, length_scale_bounds="fixed"), alpha=0.001)
         self.x = []
         self.f = []
         self.v = []
+        self.recs = []
 
 
-
+    #maybe the model works best for regret around 0.03?
     def next_recommendation(self):
         """
         Recommend the next input to sample.
@@ -45,7 +48,7 @@ class BO_algo():
         # while self.gpv.predict(x_next) <= SAFETY_THRESHOLD:
         #     x_next = self.optimize_acquisition_function()
         
-
+        self.recs.append(x_next[0])
         return x_next
 
 
@@ -66,6 +69,11 @@ class BO_algo():
             mean_v,std_v = self.gpv.predict(np.atleast_2d(x), return_std=True)
             fc = (mean_v + self.beta_v * std_v)
             return np.mean(fc)
+
+        def lowerb(x):
+            mean_v,std_v = self.gpv.predict(np.atleast_2d(x), return_std=True)
+            fc = (mean_v - self.beta_v * std_v)
+            return np.mean(fc)
         
         f_values = []
         x_values = []
@@ -74,15 +82,17 @@ class BO_algo():
         for _ in range(20):
             x0 = DOMAIN[:, 0] + (DOMAIN[:, 1] - DOMAIN[:, 0]) * \
                  np.random.rand(DOMAIN.shape[0])
-            # const = NonlinearConstraint(fun=upperb, lb=SAFETY_THRESHOLD, ub=np.inf)
-            result = fmin_l_bfgs_b(objective, x0=x0, bounds=DOMAIN,
-                                   approx_grad=True)
-            #result = minimize(objective, x0, method="L-BFGS-B", constraints=const)
+            # bounds = NonlinearConstraint(lb=lowerb(x0), ub=upperb(x0))
+            const = NonlinearConstraint(fun=upperb, lb=-np.inf, ub=SAFETY_THRESHOLD)
+            const_lo = NonlinearConstraint(fun=lowerb, lb=-np.inf, ub=SAFETY_THRESHOLD)
+            # result = fmin_l_bfgs_b(objective, x0=x0, bounds=DOMAIN, approx_grad=True)
+            # result = fmin_cobyla(objective, x0=x0, cons=upperb)
+            # x_values.append(np.clip(result[0], *DOMAIN[0]))
+            # f_values.append(-result[1])
             #print(result)
-            x_values.append(np.clip(result[0], *DOMAIN[0]))
-            f_values.append(-result[1])
-            #print(result)
-        
+            result = minimize(objective, x0, bounds=DOMAIN, constraints=const)
+            x_values.append(np.clip(result["x"], *DOMAIN[0]))
+            f_values.append(-result["fun"])
         ind = np.argmax(f_values)
         x_opt = x_values[ind].item()
         #print(x_values[ind])
@@ -112,9 +122,10 @@ class BO_algo():
         # Z = (mean - fmax - xi) / std 
         # af_value = (mean - fmax - xi) * nd.cdf(Z) + std * norm.pdf(Z)
         # print(af_value.shape)
+        # print((mean_v + std_v)[0])
         af_value = mean +self.beta * std
-        if mean_v + std_v*1e-3 >= SAFETY_THRESHOLD:
-            af_value -= (mean_v+SAFETY_THRESHOLD * 2)
+        if mean_v + std_v * self.beta_v >= SAFETY_THRESHOLD:
+            af_value -= self.beta * np.max([mean_v + std_v, 4])
 
         return np.mean(af_value)
 
@@ -160,6 +171,19 @@ class BO_algo():
         x_msk = np.array(self.x)[mask]
         ind = np.argmax(f_msk)
         return np.atleast_2d(x_msk[ind])
+    
+    def plot(self, plot_recommendation: bool = True):
+        """Plot objective and constraint posterior for debugging (OPTIONAL).
+
+        Parameters
+        ----------
+        plot_recommendation: bool
+            Plots the recommended point if True.
+        """
+        plt.ylabel("values for x")
+        plt.plot(self.x)
+        plt.plot(self.f)
+        plt.show()
 
 
 """ Toy problem to check code works as expected """
@@ -216,9 +240,10 @@ def main():
             f"shape (1, {DOMAIN.shape[0]})"
 
         # Obtain objective and constraint observation
-        obj_val = f(x) + np.randn()
-        cost_val = v(x) + np.randn()
+        obj_val = f(x) + np.random.randn()
+        cost_val = v(x) + np.random.randn()
         agent.add_data_point(x, obj_val, cost_val)
+        agent.plot()
 
     # Validate solution
     solution = agent.get_solution()
